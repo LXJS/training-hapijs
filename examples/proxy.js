db52ff4bee731969513ca2529ea8a3176afb4bfd
Wyatt Preul <wpreul@gmail.com>
2014-06-27 00:37:29 +0100

    Adding examples

---
+++ examples/proxy.js
@@ -0,0 +1,27 @@
+// Load modules
+
+var Nipple = require('nipple');
+var Hapi = require('hapi');
+
+
+// Declare internals
+
+var internals = {};
+
+
+internals.main = function () {
+
+    var server = new Hapi.Server(8000);
+
+    var mapper = function (request, callback) {
+
+        callback(null, 'http://www.google.com/search?q=' + request.params.term);
+    };
+
+    server.route({ method: '*', path: '/{p*}', handler: { proxy: { host: 'google.com', port: 80, redirects: 5 } } });
+    server.route({ method: 'GET', path: '/hapi/{term}', handler: { proxy: { mapUri: mapper } } });
+    server.start();
+};
+
+
+internals.main();
